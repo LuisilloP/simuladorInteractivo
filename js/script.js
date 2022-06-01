ea15802d55d90f2816fbe2cbc5f76b0ce3528f1c
LuisilloP <luisaraya98@hotmail.com>
2022-06-01 00:37:08 -0400

    configuracion basica

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-prompt("Hola")
+window.addEventListener('DOMContentLoaded', (event) => {
+   
+    document.getElementById('boton').addEventListener("click",()=>
+    {
+    const cantidad = document.getElementById("cantidad").value;
+    console.log(cantidad);
+    });
+});
+
+
