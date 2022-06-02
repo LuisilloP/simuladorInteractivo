b2078a26fcbb48e3337e3a83f744c0681cb1f04a
LuisilloP <luisaraya98@hotmail.com>
2022-06-01 23:06:12 -0400

    feat: Agregar Resto del codigo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,49 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-   
-    document.getElementById('boton').addEventListener("click",()=>
+
+
+    const precioJuegos =["30000","20000","40000"];
+   const btn = document.getElementById('boton');
+    const cantidadGta =  document.getElementById("cantidadGta");
+    const cantidadDbz =  document.getElementById("cantidadDbz");
+    const cantidadUnc =  document.getElementById("cantidadUnc");
+    let total = 0;
+    btn.addEventListener("click",()=>
     {
-    const cantidad = document.getElementById("cantidad").value;
-    console.log(cantidad);
-    });
+
+        if(cantidadGta.value!=0 || cantidadDbz.value !=0 || cantidadUnc.value !=0)
+        {
+            for(let i = 0; i<precioJuegos.length;i++)
+            {
+               switch (i)
+               {
+                case 0:
+                    if(cantidadDbz.value!=0)
+                    {
+                        total +=precioJuegos[i]*cantidadDbz.value;  
+                    }
+                    break;
+                case 1:
+                    if(cantidadGta.value!=0)
+                    {
+                        total +=precioJuegos[i]*cantidadGta.value;  
+                    }
+                    break;
+                case 2:
+                    if(cantidadUnc.value!=0)
+                    {
+                        total +=precioJuegos[i]*cantidadUnc.value;  
+                    }
+                    break;        
+               }
+            }
+        alert("Su tutal es de "+total);
+        total=0;
+        }else
+        {
+            alert("ingresa una cantidad de algun producto");
+        }
+
+    }); 
 });
 
 
